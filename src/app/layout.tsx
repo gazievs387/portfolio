5efbcf9fac8248de9f5b2c525ccac3b72915dff6
Said <gazievs387@gmail.com>
2025-11-13 12:15:30 +0300

    remove Suspense in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-import { Suspense } from "react";
 
 
 const inter = Inter({ subsets: ["latin"] })
@@ -32,21 +31,17 @@ export default function RootLayout({
             >
                 {children}
 
-                <Suspense>
-                    <>
-                        <Script type="text/javascript" id="id">
-                            {`(function(m,e,t,r,i,k,a){
-                                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                                m[i].l=1*new Date();
-                                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105075784', 'ym');
-                            ym(105075784, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-                            `}
-                        </Script>
-                        <noscript><div><img src="https://mc.yandex.ru/watch/105075784" style={{position: "absolute", left: "-9999px"}} alt="" /></div></noscript>
-                    </>
-                </Suspense>
+                <Script type="text/javascript" id="id">
+                    {`(function(m,e,t,r,i,k,a){
+                        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                        m[i].l=1*new Date();
+                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105075784', 'ym');
+                    ym(105075784, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+                    `}
+                </Script>
+                <noscript><div><img src="https://mc.yandex.ru/watch/105075784" style={{position: "absolute", left: "-9999px"}} alt="" /></div></noscript>
             </body>
         </html>
     );
